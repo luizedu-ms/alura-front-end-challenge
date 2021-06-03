c6efeed467a6d9cbd3bea1e037e07ab996fd5059
luizedu-ms <luedu159@gmail.com>
2021-06-03 19:59:03 -0300

    Conctruindo pagina responsiva

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function openNav() {
-    document.getElementById("mysidepanel").style.width = "35%";
+    document.getElementById("mysidepanel").style.width = "40%";
   }
   function closeNav() {
     document.getElementById("mysidepanel").style.width = "0";
+  }
+  function openSearch() {
+    document.getElementById("mytop-panel").style.height = "100%";
+    document.getElementById("myopensearch").style.visibility = "hidden";
+
+  }
+  function closeSearch() {
+    document.getElementById("mytop-panel").style.height = "0";
+    document.getElementById("myopensearch").style.visibility = "visible";
+
   }
