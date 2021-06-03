57acfa36c693b405ab88410066b5cd5da221b5a6
luizedu-ms <luedu159@gmail.com>
2021-06-03 17:42:36 -0300

    commit on 'main'

---
+++ script.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("mysidepanel").style.width = "35%";
+  }
+  function closeNav() {
+    document.getElementById("mysidepanel").style.width = "0";
+  }
